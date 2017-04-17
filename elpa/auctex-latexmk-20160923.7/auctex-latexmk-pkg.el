14134f4ae731051f4086c1c6bf99640e556dd8e8
John Ankarström <john.ankarstrom@gmail.com>
2017-04-17 19:13:15 +0200

    Re-installed AucTeX

--- elpa/auctex-latexmk-20160923.7/auctex-latexmk-pkg.el
+++ elpa/auctex-latexmk-20160923.7/auctex-latexmk-pkg.el
@@ -1,1 +1,2 @@
-(define-package "auctex-latexmk" "20160923.7" "Add LatexMk support to AUCTeX" (quote ((auctex "11.87"))))
+;;; -*- no-byte-compile: t -*-
+(define-package "auctex-latexmk" "20160923.7" "Add LatexMk support to AUCTeX" '((auctex "11.87")) :url "https://github.com/tom-tan/auctex-latexmk/" :keywords '("tex"))
