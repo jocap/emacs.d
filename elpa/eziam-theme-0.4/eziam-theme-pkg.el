841bbb05529c42143a42a0853493ab1c35a3dcf9
John Ankarström <john.ankarstrom@gmail.com>
2017-05-10 18:05:50 +0200

    Updated packages

--- elpa/eziam-theme-0.4/eziam-theme-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "eziam-theme" "0.4" "A mostly monochrome theme, inspired by Tao and Leuven, with dark and light versions." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/gruvbox-theme-20161229.1817/gruvbox-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "gruvbox-theme" "20161229.1817" "A retro-groove colour theme for Emacs" (quote ((autothemer "0.2"))))
