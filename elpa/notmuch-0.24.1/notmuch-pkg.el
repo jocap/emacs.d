7954be273fb89d7870e2ac0d295dae47920c9d0a
John Ankarström <john.ankarstrom@gmail.com>
2017-07-15 21:17:55 +0200

    Upgraded tons of packages

--- elpa/notmuch-0.24.1/notmuch-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "notmuch" "0.24.1" "run notmuch within emacs" 'nil :url "https://notmuchmail.org/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/notmuch-0.24.2/notmuch-pkg.el
@@ -0,0 +1,4 @@
+(define-package "notmuch" "0.24.2" "run notmuch within emacs" 'nil :url "https://notmuchmail.org/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
