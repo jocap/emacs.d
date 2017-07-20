45f7b964a214e989dfdda95164c8a5c068f2f1ef
John Ankarström <john.ankarstrom@gmail.com>
2017-07-21 00:08:45 +0200

    Installed ahk-mode and suggest.el

---
+++ elpa/ahk-mode-1.5.6/ahk-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ahk-mode" "1.5.6" "Major mode for editing AHK (AutoHotkey and AutoHotkey_L)" '((emacs "24.3")) :commit "bf3205efe7b7a40f3c8978f68f14ea3a939cffa8" :url "https://github.com/ralesi/ahk-mode" :keywords '("ahk" "autohotkey" "hotkey" "keyboard shortcut" "automation"))
