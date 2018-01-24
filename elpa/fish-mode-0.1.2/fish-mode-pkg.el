77eaf724b11d5473d73541cf72cf18b350ce8024
John <92702+jocap@users.noreply.github.com>
2018-01-24 13:47:02 +0100

    updated packages

--- elpa/fish-mode-0.1.2/fish-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "fish-mode" "0.1.2" "Major mode for fish shell scripts" '((emacs "24")) :commit "22aabbccd564883684f6d224b8e0a512f334be41" :keywords '("fish" "shell"))
